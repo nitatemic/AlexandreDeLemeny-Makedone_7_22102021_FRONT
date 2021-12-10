b3575312437d7258e97ed2fa4f612a718d7ac586
nitatemic <alexandre@nitatemic.dev>
2021-12-10 17:19:16 +0100

    Change import

--- src/pages/Banner.jsx
+++ src/pages/Banner.jsx
@@ -1,16 +1,6 @@
 import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
+import {AppBar, Box, Toolbar, IconButton, Typography, Menu, MenuIcon, Container, Avatar, Tooltip, MenuItem} from '@mui/material';
+
 
 import {Link} from "react-router-dom";
 import'./css/banner.css';
