8a278691861d7b3d3fbec70534a6ae28c0e36b0f
nitatemic <alexandre@nitatemic.dev>
2021-12-10 14:44:13 +0100

    Creation of the post structure in HTML

---
+++ src/components/post.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+//Un post avec un titre, un contenu, un auteur et les commentaires associés
+function Post(props) {
+  return (
+    <div className="post">
+      <h2 className="post-title">{props.Title}</h2>
+      <img className="img-fluid" src={props.Body} alt="Photo"/>  {/*Utiliser une service comme Azure computer vision pour le alt ?*/}
+      <p className="post-author">{props.Author}</p>
+      <p className="post-comments-count">{props.comments.length} comments</p>
+      <ul className="post-comments-list">
+        {props.comments.map((comment, index) => (
+          <li key={index}>
+            <p>{comment.content}</p>
+            <p>{comment.Author}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
