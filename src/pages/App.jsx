d2febf016aca0fead34f8a00850875959dff05f8
nitatemic <alexandre@nitatemic.dev>
2021-12-10 17:35:47 +0100

    Change location of navbar's'file

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "../index.css"
-import Banner from "./Banner"   //navbar
+import Banner from '../components/Banner'   //navbar
 import Login from "./Login"
 import Signup from "./Signup"
 
