f133aab74fb804e7dd05469643cf45aeaf47326e
nitatemic <alexandre@nitatemic.dev>
2021-12-16 16:57:54 +0100

    Fonction pour demander certains posts à l'API

--- src/pages/Flux.jsx
+++ src/pages/Flux.jsx
@@ -1,81 +1,23 @@
+//Page principal, qui affiche tous les posts
 import React from 'react';
 import './css/login.css'
 import {Button, TextField} from "@mui/material";
 const imgFolder = './assets/images';
 function Flux() {
     return (
-
-        <section className="vh-100">
-            <div className="container-fluid h-custom">
-                <div className="row d-flex justify-content-center align-items-center h-100">
-                    <div className="col-md-9 col-lg-6 col-xl-5">
-                        <img src = "../../assets/images/logo/svg/icon-left-font-monochrome-white.svg"
-                             className="img-fluid"
-                             alt="Logo Groupomania"/>
-                    </div>
-                    <div className="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
-                        <form>
-
-                            <div className="form-outline mb-4" >
-                                <TextField sx={{width: '100%'}} label="Prénom" variant="outlined" type="text" id="firstName" />
-                            </div>
-
-                            <div className="form-outline mb-4" >
-                                <TextField sx={{width: '100%'}} label="Nom de famille" variant="outlined" type="text" id="lastName" />
-                            </div>
-
-                            <div className="form-outline mb-4" >
-                                <TextField sx={{width: '100%'}} label="Adresse mail" variant="outlined" type="email" id="mail" />
-                            </div>
-
-                            <div className="form-outline mb-4" >
-                                <TextField sx={{width: '100%'}} label="Mot de passe" variant="outlined" type="password" id="password" />
-                            </div>
-
-
-                            <div className="text-center text-lg-start mt-4 pt-2">
-                                <Button variant="contained"  id="btnSignUp" onClick={signUp}>S'inscrire
-                                </Button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </section>
+        <h1>Test</h1>
     )
+
 }
 
-//fonction qui permet de créer un compte
+//Demander à L'API de m'envoyer les posts du numéro X à Y
 
-function signUp(e) {
-    e.preventDefault();
-    //on récupère les données du formulaire
-    const lastName = document.getElementById('lastName').value;
-    const firstName = document.getElementById('firstName').value;
-    const mail = document.getElementById('mail').value;
-    const password = document.getElementById('password').value;
-
-    //on crée un objet user
-    const user = {
-        lastName: lastName,
-        firstName: firstName,
-        mail: mail,
-        password: password
-
-    };
-    //on envoie les données au serveur
-    console.log("Envoi des données au serveur");
-    fetch('http://localhost:3001/api/auth/signup', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(user)
-    })
+function getSomePosts(from, to) {
+    return fetch(`http://localhost:8080/posts?from=${from}&to=${to}`)
         .then(response => response.json())
-        .then(data => {
-            console.log(data);
+        .then(posts => {
+            return posts;
         })
-        .catch(error => console.error(error));
 }
+
 export default Flux
